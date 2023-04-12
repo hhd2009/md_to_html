55ab20ba24cdca2df5ee11649972edbccf225236
hdlr <1079489986@qq.com>
2023-04-12 22:06:23 +0800

    2023.4.12

---
+++ js/get_file_.js
@@ -0,0 +1,17 @@
+function get_file_(url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("get", url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.response)
+      const reader = new FileReader()
+      reader.onload = function () {
+        data = reader.result
+      }
+      reader.readAsText(this.response);
+    }
+  };
+  xhr.send();
+  return data
+}
